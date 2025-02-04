e735234469c87a323c85289e21dccf6ed1dc7e11
ShauryaRawat27 <shaurya.v.rawat2704@gmail.com>
2025-02-04 17:05:25 +0530

    HEllo

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ function App() {
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      <div className="flex align-center items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{backgroundImage: "url('/image.png')"}}>
         <div className="bg-white shadow-lg rounded-lg p-8 max-w-sm w-full">
           {!user ? (
             <>
-              <h2 className="text-2xl font-bold text-center mb-4">Welcome</h2>
-              <p className="text-gray-600 text-center mb-6">Sign in with Google</p>
+              <h2 className="text-black text-2xl font-bold text-center mb-4">Welcome</h2>
+              <p className="text-gray-500 text-center mb-6">Sign in with Google</p>
               <Login setUser={setUser} />
             </>
           ) : (
